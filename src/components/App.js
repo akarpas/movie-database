54f699a0daaaf8b6b9d043c10801e85907d53a1b
Andreas Karpasitis <karpasitis@outlook.com>
2019-02-02 18:18:28 +0100

    add router - create main page

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,19 @@
 import React, { Component } from 'react';
-import MovieList from './MovieList';
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
+import MainPage from './MainPage';
 
 import style from './App.module.scss';
 
 class App extends Component {
-  state = {
-    inputValue: '',
-  }
-
-  handleChange = (e) => {
-    e.preventDefault();
-    const { value } = e.target;
-    this.setState({ inputValue: value });
-  }
-
   render() {
-    const { inputValue } = this.state;
-
     return (
       <div className={style.app}>
-        <header className={style.appHeader}>
-          <input
-            className={style.search}
-            onChange={e => this.handleChange(e)}
-            value={inputValue}
-            type="input"
-            id="search"
-            placeholder="search"
-          />
-        </header>
-        <MovieList searchTerm={inputValue} />
+        <h1>The Movie Finder</h1>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={MainPage} />
+          </Switch>
+        </BrowserRouter>
       </div>
     );
   }
