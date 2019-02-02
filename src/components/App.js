e30c4a98cd76c91dd1589267e42fc4d6f44963e0
Andreas Karpasitis <karpasitis@outlook.com>
2019-02-02 11:52:48 +0100

    connect input to state

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,23 @@ import React, { Component } from 'react';
 import style from './App.module.scss';
 
 class App extends Component {
+  state = {
+    inputValue: '',
+  }
+
+  onChange = (e) => {
+    e.preventDefault();
+    const { value } = e.target;
+    this.setState({ inputValue: value });
+  }
+
   render() {
+    const { inputValue } = this.state;
+
     return (
       <div className={style.app}>
         <header className={style.appHeader}>
-          <input className={style.search} type="input" id="search" placeholder="search" />
+          <input className={style.search} value={inputValue} type="input" id="search" placeholder="search" />
         </header>
       </div>
     );
